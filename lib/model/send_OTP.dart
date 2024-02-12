723c580162bad3a59aff63981300717be4958b09
Yogurt_with_thoughts <skrytnyjezik@gmail.com>
2024-02-12 20:19:28 +0300

    fix to send otp

--- lib/model/send_OTP.dart
+++ lib/model/send_OTP.dart
@@ -3,6 +3,12 @@ import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:supabase_flutter/supabase_flutter.dart';
+void main() async {
+  await Supabase.initialize(
+    url: 'https://hyknbwhwrzrcjgefpvhv.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5a25id2h3cnpyY2pnZWZwdmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2ODQ2NzMsImV4cCI6MjAyMjI2MDY3M30.u0RENNqmz4Ab5WmHnXHc8x-pbdlhN50fxgx6Bt-mAdE',
+  );}
 final supabase = Supabase.instance.client; 
  String generateOTP(int length) {
   var generator = Random();
--- lib/pages/session 2/signUp_page.dart
+++ lib/pages/session 2/signUp_page.dart
@@ -24,7 +24,7 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _namefullController=TextEditingController();
   bool checkedValue = false;
   
-  String get url => url;
+  String get url => 'https://hyknbwhwrzrcjgefpvhv.supabase.co';
   
   String get annonKey => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5a25id2h3cnpyY2pnZWZwdmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2ODQ2NzMsImV4cCI6MjAyMjI2MDY3M30.u0RENNqmz4Ab5WmHnXHc8x-pbdlhN50fxgx6Bt-mAdE';
   @override
